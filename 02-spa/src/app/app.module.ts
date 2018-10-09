fa0d7bfe4d31ac29d875f6736cf0aa270d2a2d83
daniel romero <raiga1234@gmail.com>
2018-10-08 22:27:41 -0500

    create services, heroes and hero, routing

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { APP_ROUTING } from './app.routes';
 
 // Servicios
 
+import { HeroesService } from './services/heroes.service';
+
 
 // Componentes
 import { AppComponent } from './app.component';
@@ -14,6 +16,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +24,16 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
