3b3eb8bb1bd29f4d9b1439c61521a496f196099b
daniel romero <raiga1234@gmail.com>
2019-01-04 18:53:47 -0500

    finish ionic application

--- 05-deseos/src/components/listas.component.ts
+++ 05-deseos/src/components/listas.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { DeseosService } from '../services/deseos.service';
 import { AgregarPage } from '../pages/agregar/agregar.component';
 import { Lista } from '../models';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ItemSliding } from 'ionic-angular';
 
 @Component({
     selector: 'app-listas',
@@ -11,9 +11,10 @@ import { NavController } from 'ionic-angular';
 export class ListasComponent {
 
     @Input() terminada: boolean = false;
-    
+
     constructor(public _deseos: DeseosService,
-        private _navCtrl: NavController) { }
+        private _navCtrl: NavController,
+        private _alertCtrl: AlertController) { }
 
     listaSeleccionada(lista: Lista) {
         this._navCtrl.push(AgregarPage, {
@@ -24,6 +25,35 @@ export class ListasComponent {
 
     borrarLista(lista: Lista) {
         this._deseos.borrarLista(lista);
+    }
+
+    editarLista(lista: Lista, slidingItem: ItemSliding) {
+        slidingItem.close();
+        const alerta = this._alertCtrl.create(
+            {
+                title: 'Editar nombre',
+                message: 'Editar el nombre de la lista',
+                inputs: [{
+                    name: 'titulo',
+                    placeholder: 'Nombre de la lista',
+                    value: lista.titulo
+                }],
+                buttons: [{
+                    text: 'Cancelar'
+                },
+                {
+                    text: 'Guardar',
+                    handler: data => {
+                        if (data.titulo === 0) {
+                            return;
+                        }
+                        lista.titulo = data.titulo;
+                        this._deseos.guardarStorage();
+                    }
+                }
+                ]
+            });
+        alerta.present();
     }
 
 }
