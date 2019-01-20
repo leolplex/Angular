d7176a2bb5971b3480358c3b5f4c0bda3f458374
daniel romero <raiga1234@gmail.com>
2019-01-20 09:42:04 -0500

    validate forms

--- 08-formularios/src/app/components/template/template.component.ts
+++ 08-formularios/src/app/components/template/template.component.ts
@@ -4,14 +4,18 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styles: []
+  styles: [`
+    .ng-invalid.ng-touched:not(form){
+      border: 1px solid red;
+    }
+  `]
 })
 export class TemplateComponent implements OnInit {
 
   usuario: Object = {
-    nombre: 'Pepe',
-    apellido: 'Mujica',
-    correo: 'pepe@monedas.com'
+    nombre: null,
+    apellido: null,
+    correo: null
   };
 
   constructor() { }
