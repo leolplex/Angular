b2e7f41a9dd6e15459c79224f5bc9af43734ad2a
daniel romero <raiga1234@gmail.com>
2019-04-15 21:31:20 -0500

    Mapa APP

---
+++ 12-mapas/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+
+@NgModule({
+  declarations: [AppComponent, MapaComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
