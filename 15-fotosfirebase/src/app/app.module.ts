ebaf5ece7f0cff50eba2bf98affed2d55f41e514
daniel romero <raiga1234@gmail.com>
2019-04-19 14:46:05 -0500

    fotos to firebase

---
+++ 15-fotosfirebase/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FeatureRoutingModule } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+@NgModule({
+  declarations: [AppComponent, FotosComponent, CargaComponent],
+  imports: [BrowserModule, FeatureRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ 15-fotosfirebase/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+const routes: Routes = [
+  { path: 'fotos', component: FotosComponent },
+  { path: 'carga', component: CargaComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'fotos' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class FeatureRoutingModule {}
